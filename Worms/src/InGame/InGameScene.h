3a2796024c35662c216a32a98cb8f4edb77e6916
GearEngine <GearEngine@gmail.com>
2020-05-06 15:08:55 +0900

    Add Intro Scene

--- Worms/src/InGame/InGameScene.h
+++ Worms/src/InGame/InGameScene.h
@@ -6,50 +6,15 @@
 
 namespace InGame {
 
-	/*inline void workFunk(InitiateData initData) 
-	{
-		int i = 1;
-		auto tempCamera = new Gear::OrthographicCameraController(initData.WindowWidth / initData.WindowHeight);
-		glm::mat4 trans = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-
-		tempCamera->GetCamera().SetPosition({ 0.0f, 0.0f, 1.0f });
-		std::cout << "Loading thread start!" << std::endl;
-		while (++i)
-		{
-			std::cout << "In Loading Thread! i : " << i << std::endl;
-			Gear::RenderCommand::SetClearColor({ 1.0f, 0.0f, 1.0f, 1.0f });
-			Gear::RenderCommand::Clear();
-
-			Gear::Renderer2D::BeginScene(tempCamera->GetCamera());
-
-			Gear::Renderer2D::DrawQuad(trans, Gear::TextureStorage::GetTexture2D("City"));
-
-			Gear::Renderer2D::EndScene();
-
-			Gear::Application::Get().GetWindow().OnUpdate();
-
-			if (i == 300)
-				return;
-		}
-		std::cout << "Loading thread end!" << std::endl;
-	}*/
-
 	class InGameScene : public Gear::Scene
 	{
 	public:
 		InGameScene(const std::string& name, Gear::Layer* imGuiLayer, const InitiateData& initData)
 			: Gear::Scene(name)
 		{
-			//std::thread LoadingThread = std::thread(workFunk, initData);
-
-			//std::thread InitThread = std::thread([&]() {
-				PushLayer(new UILayer(initData));
-				PushLayer(new BackGroundLayer(initData));
-				PushLayer(new ObjectLayer(initData));
-			//);
-
-			//LoadingThread.join();
-			//InitThread.join();
+			PushLayer(new UILayer(initData));
+			PushLayer(new BackGroundLayer(initData));
+			PushLayer(new ObjectLayer(initData));
 		}
 
 		inline virtual void Update(Gear::Timestep ts) override
